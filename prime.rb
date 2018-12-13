478f7baf11d1ab6a42fbfe4963c17b580ec2df3f
Jason Basuil <basuilj@gmail.com>
2018-12-13 06:15:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  is_prime = false 
+  case num 
+    when 0 
+  
+    else 
+      numbers = (2..num.abs).to_a 
+      factors = []
+      numbers.each do |x|
+        if num.abs % x == 0 || x == num 
+          numbers << x 
+        end 
+        is_prime = !!(factors.length == 1)
+      end 
+    end 
+  is prime  
+end
